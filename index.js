0fd90ada74eb887589c5e15533d2b7f90aee1f32
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2022-10-28 09:57:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,24 +14,18 @@ convertBtn.addEventListener("click", function(){
     // condition to make sure that if user hits Convert button without value in it he doesnt get empty output like: meters insted of 0 meters
     if(num != ""){
         // meter - feet convert with rounding
-        const mtf = num * 3.280839895
-        const rsltMTF = mtf.toFixed(3)
-        const ftm = num / 3.280839895
-        const rsltFTM = ftm.toFixed(3)
+        const mtf = (num * 3.280839895).toFixed(3)
+        const ftm = (num / 3.280839895).toFixed(3)
         // liter - gallon convert with rounding
-        const ltg = num * 0.264172052
-        const rsltLTG = ltg.toFixed(3)
-        const gtl = num / 0.264172052
-        const rsltGTL = gtl.toFixed(3)
+        const ltg = (num * 0.264172052).toFixed(3)
+        const gtl = (num / 0.264172052).toFixed(3)
         // kilograms - pounds convert with rounding
-        const ktp = num * 2.20462262
-        const rsltKTP = ktp.toFixed(3)
-        const ptk = num / 2.20462262
-        const rsltPTK = ptk.toFixed(3)
+        const ktp = (num * 2.20462262).toFixed(3)
+        const ptk = (num / 2.20462262).toFixed(3)
         //DOM manipulation
-        lengthEl.innerHTML = `${num} meters = ${rsltMTF} feet | ${num} feet = ${rsltFTM} meters`
-        volumeEl.innerHTML = `${num} liters = ${rsltLTG} gallons | ${num} gallons = ${rsltGTL} liters`
-        massEl.innerHTML = `${num} kilos = ${rsltKTP} pounds | ${num} pounds = ${rsltPTK} kilos`   
+        lengthEl.innerHTML = `${num} meters = ${mtf} feet | ${num} feet = ${ftm} meters`
+        volumeEl.innerHTML = `${num} liters = ${ltg} gallons | ${num} gallons = ${gtl} liters`
+        massEl.innerHTML = `${num} kilos = ${ktp} pounds | ${num} pounds = ${ptk} kilos`   
     }
     
 })
